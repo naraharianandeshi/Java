d578a94c840d9ac4da2017d0e59d51aece16f610
dineshnikhil <dineshkumarpokkula@gmail.com>
2023-11-29 23:12:26 +0530

    day1 in java

---
+++ Day1/Addition.java
@@ -0,0 +1,18 @@
+public class Addition {
+    public static void main(String args[]){
+        int num1=20;
+        int num2=3;
+        int sum= num1+num2;
+        int sub= num1-num2;
+        int mul= num1*num2;
+        int div= num1/num2;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+
+
+
+    }
+}
