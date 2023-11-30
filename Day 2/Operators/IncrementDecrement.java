2915df0e083fc053dd8d9fc780036a32bc32361f
naraharianandeshi <dineshkumarpokkula@gmail.com>
2023-11-30 22:34:10 +0530

    Operatos

---
+++ Day 2/Operators/IncrementDecrement.java
@@ -0,0 +1,13 @@
+package Operators;
+public class IncrementDecrement {
+    public static void main(String[] args) { 
+        
+        int a =10;
+        int b = a++;
+        int c = a--;
+        
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
+    }
+}
---
+++ Day 2/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    // gobal variable
+    // static int num1 = 12;
+
+
+    public static void hello(){
+        char ch = 'A';
+        System.out.println(ch);
+    }
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 2;
+        System.out.println(5>5);
+    }
+}
